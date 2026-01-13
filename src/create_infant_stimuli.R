676416060dc7f1097c4ee0f41217cc38fab2ca45
Connor Mayer <cjmayer@uci.edu>
2026-01-13 09:24:29 -0800

    Blah

--- src/create_infant_stimuli.R
+++ src/create_infant_stimuli.R
@@ -56,8 +56,20 @@ task <- task %>%
          bi_prob_smoothed = bi_prob_smoothed,
          group = group)
 
+vowel_stress_scores <- read_csv('phonotactic_scores/testing_data_vowel_stress_scores.csv') %>%
+  select(word, uni_prob_smoothed, bi_prob_smoothed, pos_uni_score_freq_weighted, pos_bi_score_freq_weighted) %>%
+  mutate(vowel_stress_uni_prob_smoothed = uni_prob_smoothed,
+         vowel_stress_bi_prob_smoothed = bi_prob_smoothed,
+         vowel_stress_pos_uni_score = pos_uni_score_freq_weighted,
+         vowel_stress_pos_bi_score = pos_bi_score_freq_weighted) %>%
+  distinct()
+
 task_responses <- task %>%
   filter(zone == "response_slider_endValue") %>%
+  mutate(word = str_replace_all(word, '^u', 'w')) %>%
+  mutate(word = str_replace_all(word, '^(\\w+\\d? \\w+\\d? )u', '\\1w')) %>%
+  mutate(word = str_replace_all(word, '^i', 'j')) %>%
+  mutate(word = str_replace_all(word, '^(\\w+\\d? \\w+\\d? )i', '\\1j')) %>%
   mutate(response = as.numeric(response),
          word = str_replace_all(word, 't͡ʃ', 'tʃ'),
          no_stress_word = str_replace_all(word, '\\d', ''),
@@ -65,56 +77,38 @@ task_responses <- task %>%
   # People who said Spanish was not their native language
   filter(!(ID %in% c(9642250, 9602917, 9544827, 9524953, 9751912, 976608, 9864223, 9989475)))
 
-vowel_stress_scores <- read_csv('phonotactic_scores/testing_data_vowel_stress_scores.csv') %>%
-  select(word, uni_prob_smoothed, bi_prob_smoothed, pos_uni_score, pos_bi_score) %>%
-  mutate(vowel_stress_uni_prob_smoothed = uni_prob_smoothed,
-         vowel_stress_bi_prob_smoothed = bi_prob_smoothed,
-         vowel_stress_pos_uni_score = pos_uni_score,
-         vowel_stress_pos_bi_score = pos_bi_score) %>%
-  distinct()
+task_responses %>%
+  select(ID, timestamp, zone, response, RT, trial, orthography, filename,
+         word, no_stress_word, vowel_stress_word, bucket_q, group) %>%
+  anti_join(vowel_stress_scores, by=join_by(vowel_stress_word == word))
 
+
+# Join task responses and phonotactic scores
 scored_task_responses <- task_responses %>%
   select(ID, timestamp, zone, response, RT, trial, orthography, filename,
          word, no_stress_word, vowel_stress_word, bucket_q, group) %>%
   inner_join(vowel_stress_scores, by=join_by(vowel_stress_word == word))
 
-scored_task_responses %>%
-  group_by(word) %>%
-  summarize(response = mean(response),
-            uni_prob = mean(vowel_stress_uni_prob_smoothed),
-            bi_prob_smoothed = mean(vowel_stress_bi_prob_smoothed)) %>%
-  ggplot(aes(x=uni_prob, y=response)) +
-  geom_point() +
-  geom_smooth(method='lm')
-
-scored_task_responses %>%
-  group_by(word) %>%
-  summarize(response = mean(response),
-            uni_prob = mean(vowel_stress_uni_prob_smoothed),
-            bi_prob_smoothed = mean(vowel_stress_bi_prob_smoothed)) %>%
-  # mutate(response = log(response + 0.01)) %>%
-  ggplot(aes(x=bi_prob_smoothed, y=response)) +
-  geom_point() +
-  geom_smooth(method='lm')
-
+# Scale responses within participants
 model_data <- scored_task_responses %>%
-  mutate(stress_uni_prob_smoothed = scale(vowel_stress_uni_prob_smoothed)[,1],
-         stress_bi_prob_smoothed = scale(vowel_stress_bi_prob_smoothed)[,1],
-         stress_pos_uni_score = scale(vowel_stress_pos_uni_score)[,1],
-         stress_pos_bi_score = scale(vowel_stress_pos_bi_score,)[,1]
+  mutate(vowel_stress_uni_prob_smoothed = scale(vowel_stress_uni_prob_smoothed)[,1],
+         vowel_stress_bi_prob_smoothed = scale(vowel_stress_bi_prob_smoothed)[,1],
+         vowel_stress_pos_uni_score = scale(vowel_stress_pos_uni_score)[,1],
+         vowel_stress_pos_bi_score = scale(vowel_stress_pos_bi_score,)[,1]
   ) %>%
   group_by(ID) %>%
   mutate(z_response = scale(response))
 
+# Plot scaled uni/bigram scores against each other
 model_data %>% 
-  ggplot(aes(x=stress_uni_prob_smoothed, y=stress_bi_prob_smoothed)) +
+  ggplot(aes(x=vowel_stress_uni_prob_smoothed, y=vowel_stress_bi_prob_smoothed)) +
   geom_point() +
   geom_smooth(method='lm')
 
-
+# Plot scaled responses against scaled uni and bi probs
 model_data %>%
   group_by(word) %>%
-  summarize(bi_prob = mean(stress_bi_prob_smoothed),
+  summarize(bi_prob = mean(vowel_stress_bi_prob_smoothed),
             resp = mean(z_response)) %>%
   ggplot(aes(x=bi_prob, y=resp)) +
   geom_point() +
@@ -122,7 +116,7 @@ model_data %>%
 
 
 model_data %>%
-  group_by(word) %>%
+  group_by(vowel_stress_word) %>%
   summarize(response = mean(z_response),
             uni_prob = mean(vowel_stress_uni_prob_smoothed),
             bi_prob_smoothed = mean(vowel_stress_bi_prob_smoothed)) %>%
@@ -131,40 +125,38 @@ model_data %>%
   geom_point() +
   geom_smooth(method='lm')
 
+# Build data for clustering
 cluster_data <- model_data %>%
-  group_by(word) %>%
+  group_by(vowel_stress_word) %>%
   summarize(response = mean(z_response),
             uni_prob = mean(vowel_stress_uni_prob_smoothed),
-            bi_prob_smoothed = mean(vowel_stress_bi_prob_smoothed))
+            bi_prob_smoothed = mean(vowel_stress_bi_prob_smoothed),
+            uni_pos = mean(vowel_stress_pos_uni_score),
+            bi_pos = mean(vowel_stress_pos_bi_score))
 
-# cluster_data$cluster <- anticlustering(cluster_data$bi_prob_smoothed, 2)
-# cluster_data %>% group_by(cluster) %>% summarize(foo = mean(uni_prob), bar = mean(bi_prob_smoothed), response=mean(response))
-# 
-# cluster_data %>%
-#   ggplot(aes(x=uni_prob, y=response, group=cluster, color=cluster)) +
-#   geom_point() +
-#   geom_smooth(method='lm')
-
+# Regress out bigram scores from responses and unigram scores
 m_resp_no_bi <- lm(response ~ bi_prob_smoothed, data=cluster_data)
 m_uni_no_bi <- lm(uni_prob ~ bi_prob_smoothed, data=cluster_data)
 
 m_resp_no_bi_resd <- resid(m_resp_no_bi)
 m_uni_no_bi_resd <- resid(m_uni_no_bi)
 
-model_no_bi_resd <- lm( m_resp_no_bi_resd ~ m_uni_no_bi_resd )
+# Predict responses from unigram scores with bigram regressed out
+model_no_bi_resd <- lm(m_resp_no_bi_resd ~ m_uni_no_bi_resd )
 summary(model_no_bi_resd)
 
-foo <- tibble(resp_no_bi=m_resp_no_bi_resd, uni_no_bi=m_uni_no_bi_resd)
-cluster_data <- cbind(cluster_data, foo)
+# Add residuals to cluster df
+no_bi <- tibble(resp_no_bi=m_resp_no_bi_resd, uni_no_bi=m_uni_no_bi_resd)
+cluster_data <- cbind(cluster_data, no_bi)
 
 cluster_data %>%
   ggplot(aes(x=uni_no_bi, y=resp_no_bi)) +
   geom_point() +
-  geom_text(aes(label = word))
+  geom_smooth(method='lm') + 
+  geom_text(aes(label = vowel_stress_word))
 
-
-clusters <- foo %>%
-  select(m_resp_no_bi_resd, m_uni_no_bi_resd) %>%
+clusters <- no_bi %>%
+  select(resp_no_bi, uni_no_bi) %>%
   kmeans(2)
 
 cluster_data$uni_cluster <- as_factor(clusters$cluster)
@@ -173,23 +165,31 @@ cluster_data %>%
   ggplot(aes(x=uni_prob, y=bi_prob_smoothed, group=uni_cluster, color=uni_cluster)) +
   geom_point(aes(color=uni_cluster), size = 3)
 
+cluster_data %>%
+  ggplot(aes(x=uni_no_bi, y=resp_no_bi, color = uni_cluster)) +
+  geom_point() +
+  geom_smooth(method='lm') + 
+  geom_text(aes(label = vowel_stress_word))
 
+# Regress out unigram scores
 m_resp_no_uni <- lm(response ~ uni_prob, data=cluster_data)
 m_bi_no_uni <- lm(bi_prob_smoothed ~ uni_prob, data=cluster_data)
 
 m_resp_no_uni_resd <- resid(m_resp_no_uni)
 m_bi_no_uni_resd <- resid(m_bi_no_uni)
 
-bar <- tibble(resp_no_uni=m_resp_no_uni_resd, bi_no_uni=m_bi_no_uni_resd)
-bar %>%
+no_uni <- tibble(resp_no_uni=m_resp_no_uni_resd, bi_no_uni=m_bi_no_uni_resd)
+no_uni %>%
   ggplot(aes(x=resp_no_uni, y=bi_no_uni)) +
   geom_point()
 
-cluster_data <- cluster_data %>%
-  cbind(bar)
 
-clusters <- bar %>%
-  select(m_resp_no_uni_resd, m_bi_no_uni_resd) %>%
+# Get bigram clusters
+cluster_data <- cluster_data %>%
+  cbind(no_uni)
+
+clusters <- no_uni %>%
+  select(resp_no_uni, bi_no_uni) %>%
   kmeans(2)
 
 cluster_data$bi_cluster <- as_factor(clusters$cluster)
@@ -197,7 +197,6 @@ cluster_data$bi_cluster <- as_factor(clusters$cluster)
 cluster_data %>%
   ggplot(aes(x=uni_prob, y=bi_prob_smoothed, group=bi_cluster)) +
   geom_point(size=3, aes(color=bi_cluster))
-
 
 
 clusters <- cluster_data %>%
@@ -209,26 +208,13 @@ cluster_data$both_cluster <- as_factor(clusters$cluster)
 cluster_data %>%
   ggplot(aes(x=uni_prob, y=bi_prob_smoothed, group=both_cluster)) +
   geom_point(size=3, aes(color=both_cluster))
-# clusters <- cluster_data %>%
-#   select(response, uni_prob) %>%
-#   kmeans(2)
-# 
-# cluster_data$cluster <- clusters$cluster
-# 
-# cluster_data %>%
-#   group_by(resid_cluster) %>% 
-#   summarize(mean(uni_prob),
-#             mean(bi_prob_smoothed))
-# 
-# cluster_data %>%
-#   ggplot(aes(x=uni_prob, y=response, group=cluster, color=cluster)) +
-#   geom_point() +
-#   geom_smooth(method='lm')
-# 
-# anticlustering(cluster_data$bi_prob_smoothed, 2, categories=cluster_data$cluster)
 
 cluster_data %>%
   group_by(bi_cluster) %>%
+  summarize(mean(uni_prob), mean(bi_prob_smoothed), mean(response), n())
+
+cluster_data %>%
+  group_by(uni_cluster) %>%
   summarize(mean(uni_prob), mean(bi_prob_smoothed), mean(response), n())
 
 
@@ -255,14 +241,14 @@ summary(full_pca)
 cluster_data$full_pca <- full_pca$x[,1]
 
 low_uni_contrast <- cluster_data %>%
-  slice_max(order_by=-no_bi_pca, n=66) %>%
-  mutate(exp="uni_contrast",
-         condition="low")
-
-high_uni_contrast <- cluster_data %>%
   slice_max(order_by=no_bi_pca, n=66) %>%
   mutate(exp="uni_contrast",
          condition="high")
+
+high_uni_contrast <- cluster_data %>%
+  slice_max(order_by=-no_bi_pca, n=66) %>%
+  mutate(exp="uni_contrast",
+         condition="low")
 
 uni_contrast = high_uni_contrast %>% 
   rbind(low_uni_contrast)
@@ -294,21 +280,23 @@ both_contrast <- high_both_contrast %>%
   rbind(low_both_contrast)
 
 both_contrast %>%
-  inner_join(uni_contrast, by="word") %>%
+  inner_join(uni_contrast, by="vowel_stress_word") %>%
   nrow()
 
 both_contrast %>%
-  inner_join(bi_contrast, by="word") %>%
+  inner_join(bi_contrast, by="vowel_stress_word") %>%
   nrow()
 
 uni_contrast %>%
-  inner_join(bi_contrast, by="word") %>%
+  inner_join(bi_contrast, by="vowel_stress_word") %>%
   nrow()
 
 infant_data <- both_contrast %>%
   rbind(bi_contrast) %>%
   rbind(uni_contrast) %>%
-  select(word, uni_prob, bi_prob_smoothed, response, exp, condition) %>%
+  select(vowel_stress_word, uni_prob, bi_prob_smoothed, response, exp, condition) %>%
+  mutate(vowel_stress_word = str_replace_all(vowel_stress_word, '(\\w+ \\w)1', 'ˈ\\1'),
+         vowel_stress_word = str_replace_all(vowel_stress_word, ' ', '')) %>%
   write_csv("data/infant_stimuli.csv")
 
 all_both <- both_contrast %>%
@@ -331,6 +319,13 @@ all_total <- all_both %>%
   rbind(all_bi)
 
 all_total %>%
+  filter(condition != "None") %>%
   ggplot(aes(x=uni_prob, y=bi_prob_smoothed, color=condition)) +
+  geom_point(size = 3) +
+  facet_grid(~ exp)
+
+all_total %>%
+  filter(condition != "None") %>%
+  ggplot(aes(x=uni_pos, y=bi_pos, color=condition)) +
   geom_point(size = 3) +
   facet_grid(~ exp)
--- src/generate_new_transcriptions.R
+++ src/generate_new_transcriptions.R
@@ -3,7 +3,7 @@ library(tidyverse)
 espal <- read_csv("data/subtlex_esp_espal_data.csv")
 
 transcribed_data <- espal %>%
-  select(-X) %>%
+  select(-...1, -X) %>%
   mutate(transcription = map_chr(
     str_split(sa_phon_structure, pattern = ""), 
     str_flatten, collapse=" "
@@ -14,15 +14,15 @@ transcribed_data <- espal %>%
   mutate(transcription=str_replace_all(transcription, 's', 's')) %>%
   mutate(transcription=str_replace_all(transcription, 'S', 'ʃ')) %>%
   mutate(transcription=str_replace_all(transcription, 'J', 'j')) %>%
-  mutate(transcription=str_replace_all(transcription, 'y', 'j')) %>%
+  mutate(transcription=str_replace_all(transcription, 'H', 'j')) %>%
   mutate(transcription=str_replace_all(transcription, 'r', 'ɾ')) %>%
   mutate(transcription=str_replace_all(transcription, 'R', 'r')) %>%
   mutate(transcription=str_replace_all(transcription, 'N', 'ɲ')) %>%
   mutate(transcription=str_replace_all(transcription, 'G', 'g')) %>%
   mutate(transcription=str_replace_all(transcription, 'L', 'j')) %>%
   mutate(transcription=str_replace_all(transcription, 'C', 'tʃ')) %>%
+  mutate(transcription=str_replace_all(transcription, 'z', 's')) %>%
   mutate(transcription_str = str_replace(transcription, str_glue("^(([^aieou]*[aeiou]){{{sa_syll_accent}}})"), "\\11")) %>%
-  filter(!(word %in% c("nietzsche", "software", "etc", "little"))) %>%
   distinct() %>%
   mutate(freq_per_mil = (cnt / sum(cnt)) * 1000000) %>%
   select(-frq)
@@ -34,4 +34,30 @@ all_characters <- strsplit(combined_string, split = "")[[1]]
 # Get the unique characters
 unique_characters <- unique(all_characters)
 # Print the unique characters
-print(unique_characters)
+print(sort(unique_characters))
+
+transcribed_data %>% 
+  select(word, cnt, freq_per_mil, transcription) %>%
+  write_csv('data/full_espal_data.csv')
+
+transcribed_data %>%
+  filter(freq_per_mil > 1) %>%
+  select(transcription, cnt) %>%
+  write_csv('data/training/training_data_no_stress_new.csv', col_names = FALSE)
+
+transcribed_data %>%
+  filter(freq_per_mil > 1) %>%
+  select(transcription_str, cnt) %>%
+  write_csv('data/training/training_data_vowel_stress_new.csv', col_names=FALSE)
+
+test_items <- read_csv('data/testing/testing_data_no_stress.csv', col_names = FALSE)
+test_items_str <- read_csv('data/testing/testing_data_vowel_stress.csv', col_names = FALSE)
+
+# Combine all strings in column R into a single string
+combined_string <- paste0(test_items$X1, collapse = "")
+# Split the combined string into individual characters
+all_characters <- strsplit(combined_string, split = "")[[1]]
+# Get the unique characters
+unique_characters <- unique(all_characters)
+# Print the unique characters
+print(sort(unique_characters))
